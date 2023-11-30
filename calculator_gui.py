2594bade789fa49e098fd1c2f226f442bce481f5
Enes Çakmak <cakmakkeness@gmail.com>
2023-11-30 17:32:36 +0300

    Added different entry field to represent calculations better

--- calculator_gui.py
+++ calculator_gui.py
@@ -2,56 +2,50 @@ from tkinter import *
 from tkinter import ttk
 
 
+
 class Calculator:
-    def __init__(self, master):
+    def __init__(self, master, calculator_logic):
         self.master = master
         master.title("Calculator")
         master.config(padx=4, pady=2)
         master.minsize(width=300, height=350)
         master.resizable(False, False)
 
-        self.create_gui()
+        self.calculator_logic = calculator_logic
 
-    def create_gui(self):
         frame = ttk.Frame(self.master, width=300, height=50, style="TFrame")
         frame.grid(row=0, column=0, columnspan=4, padx=0, pady=0)
 
-        entry = Entry(frame, width=20, font="Arial 20 bold", justify="right", state="readonly",
-                      background="white", borderwidth=0)
+        self.result_entry = Entry(frame, width=20, font="Arial 12", textvariable=StringVar(), justify="right",
+                                  state="readonly", background="white", borderwidth=0, highlightthickness=0)
+        self.result_entry.grid(row=0, column=0, columnspan=4, padx=0, pady=0)
 
-        entry.grid(row=0, column=0, columnspan=4, padx=0, pady=0)
+        self.entry = Entry(frame, width=20, font="Arial 20 bold", textvariable=StringVar(), justify="right",
+                           state="readonly", background="white", borderwidth=0, highlightthickness=0)
+        self.entry.grid(row=1, column=0, columnspan=4, padx=0, pady=0)
 
-        ttk.Button(self.master, text="%", width=6, padding=15).grid(row=1, column=0, padx=0, pady=0)
-        ttk.Button(self.master, text="CE", width=6, padding=15).grid(row=1, column=1, padx=0, pady=0)
-        ttk.Button(self.master, text="C", width=6, padding=15).grid(row=1, column=2, padx=0, pady=0)
-        ttk.Button(self.master, text="⌫", width=6, padding=15).grid(row=1, column=3, padx=0, pady=0)
-        ttk.Button(self.master, text="1/x", width=6, padding=15).grid(row=2, column=0, padx=0, pady=0)
-        ttk.Button(self.master, text="x²", width=6, padding=15).grid(row=2, column=1, padx=0, pady=0)
-        ttk.Button(self.master, text="√x", width=6, padding=15).grid(row=2, column=2, padx=0, pady=0)
-        ttk.Button(self.master, text="÷", width=6, padding=15).grid(row=2, column=3, padx=0, pady=0)
-        ttk.Button(self.master, text="7", width=6, padding=15).grid(row=3, column=0, padx=0, pady=0)
-        ttk.Button(self.master, text="8", width=6, padding=15).grid(row=3, column=1, padx=0, pady=0)
-        ttk.Button(self.master, text="9", width=6, padding=15).grid(row=3, column=2, padx=0, pady=0)
-        ttk.Button(self.master, text="x", width=6, padding=15).grid(row=3, column=3, padx=0, pady=0)
-        ttk.Button(self.master, text="4", width=6, padding=15).grid(row=4, column=0, padx=0, pady=0)
-        ttk.Button(self.master, text="5", width=6, padding=15).grid(row=4, column=1, padx=0, pady=0)
-        ttk.Button(self.master, text="6", width=6, padding=15).grid(row=4, column=2, padx=0, pady=0)
-        ttk.Button(self.master, text="-", width=6, padding=15).grid(row=4, column=3, padx=0, pady=0)
-        ttk.Button(self.master, text="1", width=6, padding=15).grid(row=5, column=0, padx=0, pady=0)
-        ttk.Button(self.master, text="2", width=6, padding=15).grid(row=5, column=1, padx=0, pady=0)
-        ttk.Button(self.master, text="3", width=6, padding=15).grid(row=5, column=2, padx=0, pady=0)
-        ttk.Button(self.master, text="+", width=6, padding=15).grid(row=5, column=3, padx=0, pady=0)
-        ttk.Button(self.master, text="+/-", width=6, padding=15).grid(row=6, column=0, padx=0, pady=0)
-        ttk.Button(self.master, text="0", width=6, padding=15).grid(row=6, column=1, padx=0, pady=0)
-        ttk.Button(self.master, text=".", width=6, padding=15).grid(row=6, column=2, padx=0, pady=0)
-        ttk.Button(self.master, text="=", width=6, padding=15).grid(row=6, column=3, padx=0, pady=0)
 
+
+    def on_button_click(self, button_text):
+        self.calculator_logic.button_click(button_text)
+        self.update_entry()
+
+    def update_entry(self):
+        equation = self.calculator_logic.get_equation()
+        self.result_entry.config(state="normal")
+        self.result_entry.delete(0, END)
+        self.result_entry.insert(0, equation)
+
+        self.entry.config(state="normal")
+        self.entry.delete(0, END)
+        self.entry.insert(0, equation)
+        self.entry.config(state="readonly")
 
 def main():
     root = Tk()
-    app = Calculator(root)
+    calculator_logic = CalculatorLogic()
+    app = Calculator(root, calculator_logic)
     root.mainloop()
-
 
 if __name__ == "__main__":
     main()
